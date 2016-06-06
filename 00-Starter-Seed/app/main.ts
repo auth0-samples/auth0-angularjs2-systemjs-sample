000238f65a6d188090d524f3f8a71cda293d42f4
Alejandro Mataloni <amataloni@gmail.com>
2016-06-06 15:27:00 -0300

    add router to basic structure app

--- 00-Starter-Seed/app/main.ts
+++ 00-Starter-Seed/app/main.ts
@@ -1,4 +1,11 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {provide} from '@angular/core';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {RouteConfig, ROUTER_PROVIDERS, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+
 import {AppComponent} from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, { useClass: HashLocationStrategy })
+])
