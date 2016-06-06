f72cb09317835c4a35fa156827f6d58ba21525ac
Alejandro Mataloni <amataloni@gmail.com>
2016-06-06 15:27:00 -0300

    [#120861611] add router to basic structure app

---
+++ 00-Starter-Seed/app/home.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'home',
+  template: `
+    <h1>Welcome to auth0 - Angular 2 integration app</h1>
+  `
+})
+export class Home {}
