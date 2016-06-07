1f54a65bd60e3baf47401b164a9f3db704fdfcb8
Alejandro Mataloni <amataloni@gmail.com>
2016-06-07 10:56:34 -0300

    login sample app

---
+++ 01-Login/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {RouteConfig, ROUTER_PROVIDERS, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+
+import {Home} from './home.component';
+import {Auth} from './auth.service';
+
+@Component({
+    selector: 'my-app',
+    providers: [ Auth ],
+    directives: [ ROUTER_DIRECTIVES ],
+    templateUrl: 'app/app.template.html',
+    styles: [`.btn-margin { margin-top: 5px}`]
+})
+@RouteConfig([
+  { path: '/home', name: 'Home', component: Home, useAsDefault: true}
+])
+
+export class AppComponent {
+  constructor(private auth: Auth) {}
+}
