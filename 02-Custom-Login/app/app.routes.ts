67c1153473140ccd67d74daa110a802d0b440eb7
Amaan Cheval <amaan.cheval@gmail.com>
2016-09-13 12:14:14 +0530

    Update sample 02 to Angular 2 rc6

--- 02-Custom-Login/app/app.routes.ts
+++ 02-Custom-Login/app/app.routes.ts
@@ -1,13 +1,18 @@
-import { provideRouter, RouterConfig } from '@angular/router';
+import { ModuleWithProviders }         from '@angular/core';
+import { Routes, RouterModule }        from '@angular/router';
+
 import { HomeComponent }               from './home.component';
 import { LoginComponent }              from './login.component';
 
-export const routes: RouterConfig = [
+
+const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' }
 ];
 
-export const APP_ROUTER_PROVIDERS = [
-  provideRouter(routes)
+export const appRoutingProviders: any[] = [
+
 ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
