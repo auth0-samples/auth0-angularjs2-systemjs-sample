ff243bc8d5c49bb16aeb340818ebf675203b60f0
Amaan Cheval <amaan.cheval@gmail.com>
2016-09-13 12:38:51 +0530

    Update sample 03 to Angular 2 rc6

--- 03-Session-Handling/app/app.component.ts
+++ 03-Session-Handling/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component }         from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Auth }              from './auth.service';
 
 @Component({
     selector: 'my-app',
     providers: [ Auth ],
-    directives: [ ROUTER_DIRECTIVES ],
     templateUrl: 'app/app.template.html'
 })
 
