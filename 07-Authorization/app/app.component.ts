2c0e58a23ac1c39d7396f5e936050a90c426789b
Amaan Cheval <amaan.cheval@gmail.com>
2016-09-13 15:14:16 +0530

    Update sample 07 to Angular 2 rc6

--- 07-Authorization/app/app.component.ts
+++ 07-Authorization/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component }         from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Auth }              from './auth.service';
 
 @Component({
     selector: 'my-app',
-    directives: [ ROUTER_DIRECTIVES ],
     templateUrl: 'app/app.template.html'
 })
 
--- 07-Authorization/app/main.ts
+++ 07-Authorization/app/main.ts
@@ -1,14 +1,4 @@
-import { bootstrap }                              from '@angular/platform-browser-dynamic';
-import { provide }                                from '@angular/core';
-import { APP_ROUTER_PROVIDERS }                   from './app.routes';
-import { HTTP_PROVIDERS }                         from '@angular/http';
-import { AUTH_PROVIDERS }                         from 'angular2-jwt';
-import { AppComponent }                           from './app.component';
-import { Auth }                                   from './auth.service';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule }              from './app.module';
 
-bootstrap(AppComponent, [
-  HTTP_PROVIDERS,
-  APP_ROUTER_PROVIDERS,
-  AUTH_PROVIDERS,
-  Auth
-]);
+platformBrowserDynamic().bootstrapModule(AppModule);
