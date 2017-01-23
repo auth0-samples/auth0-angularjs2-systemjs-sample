1476725ef12a0918b6e59011d1c8df9921c47c60
Ryan Chenkie <ryanchenkie@gmail.com>
2017-01-23 11:55:30 -0500

    update form

--- 02-Custom-Login/app/login.component.ts
+++ 02-Custom-Login/app/login.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { Auth }      from './auth.service';
+import { Auth } from './auth.service';
 
 @Component({
   selector: 'login',
   templateUrl: 'app/login.template.html'
 })
-
 export class LoginComponent {
   constructor(private auth: Auth) {}
 };
