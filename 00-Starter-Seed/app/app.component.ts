a91c80b91f2940670a1a8c5d957042d3f7b7b2fa
Ryan Chenkie <ryanchenkie@gmail.com>
2017-05-24 13:32:31 -0400

    deprecate

--- 00-Starter-Seed/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component }          from '@angular/core';
-
-@Component({
-    selector: 'my-app',
-    templateUrl: 'app/app.template.html'
-})
-
-export class AppComponent {}
--- 04-User-Profile/app/profile.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component }         from '@angular/core';
-
-@Component({
-  selector: 'profile',
-  templateUrl: 'app/profile.template.html'
-})
-
-export class ProfileComponent {}
